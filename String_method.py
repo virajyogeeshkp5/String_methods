693f4067be14b1941244d5974e0fa8ba2f599ff1
YOGEESH K P <yogeesh793@gmail.com>
2024-09-15 12:46:29 +0530

    Add files via upload

---
+++ String_method.py
@@ -0,0 +1,96 @@
+#String Method
+
+#capitalize
+p="Hello World"
+v=p.capitalize()
+print(v)
+
+#casefold
+p="Hello World"
+v=p.casefold()
+print(v)
+
+#count
+p="Hello World"
+v=p.count('o')
+print(v)
+
+#find
+p="Hello World"
+v=p.find('World')
+print(v)
+
+#index
+p="Hello World"
+v=p.index('Hello')
+print(v)
+
+#isalnum
+p="Hello World"
+v=p.isalnum()
+print(v)
+
+#isalpha
+p="Hello World"
+v=p.isalpha()
+print(v)
+
+#isascii
+p="Hello World"
+v=p.isascii()
+print(v)
+
+#isdecimal
+p="Hello World"
+v=p.isdecimal()
+print(v)
+
+#isdigit
+p="Hello World"
+v=p.isdigit()
+print(v)
+
+#isidentifier
+p="Hello World"
+v=p.isidentifier()
+print(v)
+
+#islower
+p="Hello World"
+v=p.islower()
+print(v)
+
+#isnumeric
+p="Hello World"
+v=p.isnumeric()
+print(v)
+
+#isprintable
+p="Hello World"
+v=p.isprintable()
+print(v)
+
+#isupper
+p="hello world"
+v=p.isupper()
+print(v)
+
+#Split
+p="hello world"
+v=p.split()
+print(v)
+
+#title
+p="hello world"
+v=p.title()
+print(v)
+
+#strip
+p="hello world"
+v=p.strip()
+print(v)
+
+#replace('a','d')
+p="abc"
+v=p.replace("a","d")
+print(v)
